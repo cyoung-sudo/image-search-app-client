8945ab4f2afe663c851122a72a3da12a059df330
Christopher Young <christopheryoung@MacBook123.local>
2022-10-19 22:30:13 -0700

    updated app icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer(props) {
   return (
     <div id="footer">
-      <div>Footer</div>
+      <a href="https://www.flaticon.com/free-icons/gallery" title="gallery icons" target="_blank" rel="noreferrer">Gallery icons created by Phoenix Group - Flaticon</a>
     </div>
   );
 };
